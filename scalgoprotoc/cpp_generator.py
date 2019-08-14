scalgo/scalgoproto

commit 67bd015588023aa1195e362f42e4c6f2ff91b4bc

Accessing a missing list returns the empty list

scalgoprotoc/cpp_generator.py
@@ -135,7 +135,9 @@ class Generator:
         self.o("\t")
         self.output_doc(node, "\t")
         self.o("\tscalgoproto::ListIn<%s> %s() const {" % (typeName, lname))
-        self.o("\t\tassert(has%s());" % uname)
+        self.o("\tif (!has%s()) {" % uname)
+        self.o("\t\treturn scalgoproto::ListIn<%s>(reader_);" % typeName)
+        self.o("\t}")
         self.o(
             "\t\treturn getObject_<scalgoproto::ListIn<%s> >(reader_, getPtr_<%s, scalgoproto::LISTMAGIC, %d, scalgoproto::ListAccess<%s>::mult>());"
             % (typeName, bs(node.inplace), node.offset, typeName)

lib/cpp/scalgoproto.hh
@@ -569,6 +569,8 @@ public:
 	using size_type = std::size_t;
 	using iterator = ListInIterator<T>;
 
+	ListIn(const Reader & reader) noexcept : reader_(reader), start_(nullptr), size_(0) {}
+
 	bool hasFront() const noexcept { return !empty() && has(0); }
 	value_type front() const noexcept(noexceptGet) {
 		assert(hasFront());

scalgoprotoc/python_generator.py
@@ -180,7 +180,8 @@ class Generator:
         self.o("    @property")
         self.o("    def %s(self) -> scalgoproto.ListIn[%s]:" % (uname, tn))
         self.output_doc(node, "        ")
-        self.o("        assert self.has_%s" % uname)
+        self.o("        if not self.has_%s:" % uname)
+        self.o("            return []")
         self.o(acc)
         self.o()
 

test/test_base.cc
@@ -227,6 +227,7 @@ int main(int, char ** argv) {
 
 		REQUIRE(s.hasIntList(), true);
 		REQUIRE(s.hasNintList(), false);
+		REQUIRE(s.nintList().size(), 0);
 		auto l = s.intList();
 		auto rl = s.intListRaw();
 

test/test_base.py
@@ -489,6 +489,8 @@ def test_in_complex(path: str) -> bool:
         return False
     if require(s.has_nint_list, False):
         return False
+    if require(len(s.nint_list), 0):
+        return False
     l = s.int_list
 
     if require(len(l), 31):

commit fb7d48b8e39e5c4128053d703be522eb0c5fc387

Fix indentation in C++ codegen
Fixes: 67bd015 ("Accessing a missing list returns the empty list", 2019-08-14)

scalgoprotoc/cpp_generator.py
@@ -135,9 +135,9 @@ class Generator:
         self.o("\t")
         self.output_doc(node, "\t")
         self.o("\tscalgoproto::ListIn<%s> %s() const {" % (typeName, lname))
-        self.o("\tif (!has%s()) {" % uname)
-        self.o("\t\treturn scalgoproto::ListIn<%s>(reader_);" % typeName)
-        self.o("\t}")
+        self.o("\t\tif (!has%s()) {" % uname)
+        self.o("\t\t\treturn scalgoproto::ListIn<%s>(reader_);" % typeName)
+        self.o("\t\t}")
         self.o(
             "\t\treturn getObject_<scalgoproto::ListIn<%s> >(reader_, getPtr_<%s, scalgoproto::LISTMAGIC, %d, scalgoproto::ListAccess<%s>::mult>());"
             % (typeName, bs(node.inplace), node.offset, typeName)