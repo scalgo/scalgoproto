f2bd5d027bbf630563a9793d673a85cb440b5bf6
Mathias Rav <mathias@scalgo.com>
2020-04-06 13:31:05 +0200

    C++ lib: Use static_assert to require hasAdd in ListOut::add(i, v...)

--- lib/cpp/scalgoproto.hh
+++ lib/cpp/scalgoproto.hh
@@ -749,6 +749,7 @@ public:
 	template <typename... TT>
 	T add(size_t index, TT... vv) {
 		assert(index < size_);
+		static_assert(A::hasAdd);
 		if constexpr (A::hasAdd)
 			return A::add(writer_, offset_, index, std::forward<TT>(vv)...);
 		else {
